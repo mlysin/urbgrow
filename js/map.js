d64ddf42152044b49e38b8585286d4b7ef85788b
Alex Mandel <aimandel@ucdavis.edu>
2015-12-02 20:28:24 -0800

    Tinkering with browser location api

--- js/map.js
+++ js/map.js
@@ -1,8 +1,23 @@
-var map;
+var map,
+    userloc = {"lat": 38.5725, "lon": -121.4680, "acc": 0};
+
+function GetLocation(location) {
+    "use strict";
+    // Get the location from the web browser if user approves
+    userloc = {"lat": location.coords.latitude, "lon": location.coords.longitude, "acc": location.coords.accuracy};
+    // alert(loc);
+}
+
 
 $(document).ready(function () {
-
-    map = L.map('map').setView([38.5725, -121.4680], 13);
+    "use strict";
+    // Get the location from the web browser if user approves
+    navigator.geolocation.getCurrentPosition(GetLocation);
+    
+    // Initialize the map
+    //map = L.map('map').setView([38.5725, -121.4680], 13);
+    map = L.map('map').setView([userloc.lat, userloc.lon], 13);
+    
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
