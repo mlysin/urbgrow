bdc5c8e525feec576ba00c7823e8f2a49c329790
Alex Mandel <aimandel@ucdavis.edu>
2015-12-02 16:41:36 -0800

    Starting with Skeleton JS, adding in Leaflet

---
+++ js/map.js
@@ -0,0 +1,9 @@
+var map = L.map('map').setView([38.5725, -121.4680], 13);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([38.5725, -121.4680]).addTo(map)
+    .bindPopup('An Urban Farm<br> Grow!')
+    .openPopup();
