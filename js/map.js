ad61e07ddeff34ea746904793202a258a9eed866
Alex Mandel <aimandel@ucdavis.edu>
2015-12-02 21:15:42 -0800

    Browser location is not reliable for init of the map

--- js/map.js
+++ js/map.js
@@ -1,5 +1,19 @@
 var map,
-    userloc = {"lat": 38.5725, "lon": -121.4680, "acc": 0};
+    userloc = {"lat": 38.5725, "lon": -121.4680, "acc": 0},
+    locoptions = {timeout: 5};
+
+function initMap() {
+    "use strict";
+    map = L.map('map').setView([userloc.lat, userloc.lon], 13);
+    
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([38.5725, -121.4680]).addTo(map)
+        .bindPopup('An Urban Farm<br> Grow!')
+        .openPopup();
+}
 
 function GetLocation(location) {
     "use strict";
@@ -8,23 +22,36 @@ function GetLocation(location) {
     // alert(loc);
 }
 
+function NoLocation(error) {
+    "use strict";
+    userloc = {"lat": 38.5725, "lon": -121.4680, "acc": 0};
+    initMap();
+    switch (error.code) {
+    case error.PERMISSION_DENIED:
+        userloc = {"lat": 38.5725, "lon": -121.4680, "acc": 0};
+        initMap();
+        break;
+    case error.POSITION_UNAVAILABLE:
+        userloc = {"lat": 38.5725, "lon": -121.4680, "acc": 0};
+        initMap();
+        break;
+    case error.TIMEOUT:
+        userloc = {"lat": 38.5725, "lon": -121.4680, "acc": 0};
+        initMap();
+        break;
+    case error.UNKNOWN_ERROR:
+        userloc = {"lat": 38.5725, "lon": -121.4680, "acc": 0};
+        initMap();
+        break;
+    }
+}
 
 $(document).ready(function () {
     "use strict";
     // Get the location from the web browser if user approves
-    navigator.geolocation.getCurrentPosition(GetLocation);
+    //navigator.geolocation.getCurrentPosition(GetLocation, NoLocation, locoptions);
     
     // Initialize the map
     //map = L.map('map').setView([38.5725, -121.4680], 13);
-    map = L.map('map').setView([userloc.lat, userloc.lon], 13);
-    
-
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    L.marker([38.5725, -121.4680]).addTo(map)
-        .bindPopup('An Urban Farm<br> Grow!')
-        .openPopup();
-    
+    initMap();
 });
